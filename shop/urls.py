1a2cd9d49f4c18c4a12eae7cf315256ca937f26f
jasurmavlonov02 <jasurmavlonov24@gmail.com>
2025-05-08 16:34:31 +0500

    search

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,7 @@ from .views import index,product_detail
 
 urlpatterns = [
     path('',index,name='index'),
-    path('detail/<int:product_id>/',product_detail,name='product_detail')
+    path('category/<int:category_id>/',index, name='products_by_category'),
+    path('detail/<int:product_id>/',product_detail,name='product_detail'),
 ]
 
--- shop/views.py
+++ shop/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product,Category
 from django.http.response import HttpResponse
 
 # Create your views here.
 
 
-def index(request):
-    products = Product.objects.all()
+def index(request,category_id=None):
+    search_query = request.GET.get('q','')
+    categories = Category.objects.all()
+    
+    
+    
+    
+    if category_id:
+        products = Product.objects.filter(category = category_id)
+    else:
+        products = Product.objects.all()
+        
+    if search_query:
+        products = products.filter(name__icontains = search_query)
+    
+    
     context = {
         'products': products,
+        'categories': categories
     }
     return render(request, 'shop/home.html',context)
 
